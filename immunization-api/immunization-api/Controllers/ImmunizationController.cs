06861089b1fcac3b56abc7698f073e75ffb2293b
bradhead <brad@zedwerks.com>
2021-04-01 16:10:53 -0700

    updated
    Signed-off-by: bradhead <brad@zedwerks.com>

--- immunization-api/immunization-api/Controllers/ImmunizationController.cs
+++ immunization-api/immunization-api/Controllers/ImmunizationController.cs
@@ -1,21 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-
-using ImmunizationApi.Models;
-
+﻿//-------------------------------------------------------------------------
+// Copyright © 2021 Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-------------------------------------------------------------------------
 namespace ImmunizationApi.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    using ImmunizationApi.Models;
     /// <summary>
     /// The Immunization controller.
     /// </summary>
 
     //[Authorize]
-    [ApiVersion("1.0")]
-    [Route("v{version:apiVersion}/api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ImmunizationController : ControllerBase
     {
@@ -25,7 +42,7 @@ namespace ImmunizationApi.Controllers
         /// <summary>
         /// Gets or sets the immunization data service.
         /// </summary>
-        private readonly IImmunizationDataService dataService;
+        //private readonly IImmunizationDataService dataService;
 
         public ImmunizationController(ILogger<ImmunizationController> logger)
         {

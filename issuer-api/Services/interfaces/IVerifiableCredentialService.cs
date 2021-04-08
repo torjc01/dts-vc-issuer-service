af55201348aa471dc43b98abb7c738526aecb5fe
Anais <anais.hebert@nttdata.com>
2021-04-08 16:22:27 -0700

    connections

--- issuer-api/Services/interfaces/IVerifiableCredentialService.cs
+++ issuer-api/Services/interfaces/IVerifiableCredentialService.cs
@@ -2,12 +2,13 @@ using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 
 using Issuer.Models;
+using System.Collections.Generic;
 
 namespace Issuer.Services
 {
     public interface IVerifiableCredentialService
     {
-        Task<Connection> CreateConnectionAsync(Patient patient, string[] urls);
+        Task<int> IssueCredentialsAsync(Patient patient, List<Identifier> identifiers);
         Task<bool> WebhookAsync(JObject data, string topic);
         Task<bool> RevokeCredentialsAsync(int patientId);
     }

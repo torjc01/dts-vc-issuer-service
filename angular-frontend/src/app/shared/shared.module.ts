bec9a8f0de5e8e67057ac5fd4c470b6357d3ff04
Martin Pultz <mtpultz@gmail.com>
2021-04-11 16:35:36 -0700

    UI updates and issuing credential

--- angular-frontend/src/app/shared/shared.module.ts
+++ angular-frontend/src/app/shared/shared.module.ts
@@ -5,21 +5,31 @@ import { RouterModule } from '@angular/router';
 
 import { NgxMaterialModule } from './modules/ngx-material/ngx-material.module';
 import { FormatDatePipe } from './pipes/format-date.pipe';
+import { SafePipe } from './pipes/safe.pipe';
+import { CardAlertComponent } from './components/card-alert/card-alert.component';
+import { ImageComponent } from './components/image/image.component';
 
 @NgModule({
   declarations: [
-    FormatDatePipe
+    FormatDatePipe,
+    SafePipe,
+    CardAlertComponent,
+    ImageComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+    NgxMaterialModule
   ],
   exports: [
     CommonModule,
     ReactiveFormsModule,
     NgxMaterialModule,
-    FormatDatePipe
+    FormatDatePipe,
+    SafePipe,
+    CardAlertComponent,
+    ImageComponent
   ]
 })
 export class SharedModule { }

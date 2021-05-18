39ddfa62f3b8e9f03b36d471d33bdb2e0564631a
torjc01 <julio_cesar.torres@sct.gouv.qc.ca>
2021-05-18 13:34:00 -0400

    Add user directions to landing page
    Signed-off-by: torjc01 <julio_cesar.torres@sct.gouv.qc.ca>

--- verifier/voyages-controller/src/components/LandingPageComponent.js
+++ verifier/voyages-controller/src/components/LandingPageComponent.js
@@ -41,17 +41,27 @@ const LandingPageComponent = () => {
       <div className="pt-5 container-fluid text-center" >
         <div className="row" >
           <div className="col-md-7 col-sm-12" style={{ 'paddingTop': '0' }}>
-            <h1 className="mt-5">{t('vaccine:travelCarefreeTitle')}</h1>
-            <p>{t('vaccine:travelCarefreeSubTitle')}</p>
-            <p className="lead">
-            {t('vaccine:travelCarefreeDescription')}</p>
+            <h1 className="mt-5" style={{ paddingBottom: "40px"}}>{t('vaccine:travelCarefreeTitle')}</h1>
             
+            <p className="sublead"> {t('vaccine:travelCarefreeDescription')} </p>
+            
+            <p className="sublead">{t('vaccine:directions1')}</p>
+
+            <ol className="sublead">
+              <li>{t('vaccine:directions2')}</li>
+              <li>{t('vaccine:directions3')}</li>
+              <li>{t('vaccine:directions4')}</li>
+              <li>{t('vaccine:directions5')}</li>
+              <li>{t('vaccine:directions6')}</li>
+            </ol>
+
             <Button className="mt-5" size="lg" color="primary" onClick={() => {
                 createInvitation();
             }}>{t('vaccine:tryOnBookingButtonLabel')}</Button>
             
           </div>
-          <div class="col-md-4 col-sm-12">
+
+          <div className="col-md-4 col-sm-12">
             <img src={bannerImg} alt="CovidPerson" />
           </div>
         </div>
--- verifier/voyages-controller/src/helpers/NavigationHelpers.js
+++ verifier/voyages-controller/src/helpers/NavigationHelpers.js
@@ -1,5 +1,8 @@
-import history from './History'
-
-export function routePush(route) {
-    history.push(route)
+/**
+ * @description 
+ * Forwards to issuer page. 
+ */
+export function returnIssuer(){
+    console.log("Back to issuer"); 
+    window.open("http://localhost:4200/issuer/credentials", "_self");
 }
